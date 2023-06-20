808680bdf76dec19479b8992d4d55266cbe498f4
tjtharrison <gh@tjth.co>
2023-06-20 21:30:41 +0100

    feat: Added some CI and started building out action to deploy changed files

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
-import requests
+"""Upload articles to medium"""
 import os
-import json
+
+import requests
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -17,11 +18,11 @@ def get_user_info():
          user_detail
     """
     my_details = requests.get(
-        url=f"https://api.medium.com/v1/me?accessToken={api_token}",
-        timeout=10
+        url=f"https://api.medium.com/v1/me?accessToken={api_token}", timeout=10
     ).json()
 
     return my_details
+
 
 def post_article(contents, state="draft"):
     """
@@ -34,27 +35,70 @@ def post_article(contents, state="draft"):
     Returns:
         True if successful
     """
-    headers = {
-        "Authorization": f"Bearer {api_token}"
-    }
+    print("Posting article")
+    try:
+        headers = {"Authorization": f"Bearer {api_token}"}
 
-    article_metadata = {
-        "title": "Hello World",
-        "contentFormat": "markdown",
-        "content": "# Hello World\nSome example text",
-        "tags": ["helloworld", "testing"],
-        "publishStatus": state
-    }
+        article_metadata = {
+            "title": "Hello World",
+            "contentFormat": "markdown",
+            "content": contents,
+            "tags": ["helloworld", "testing"],
+            "publishStatus": state,
+        }
 
-    response = requests.post(
-        url=f'https://api.medium.com/v1/users/{get_user_info()["data"]["id"]}/posts',
-        headers=headers,
-        timeout=10,
-        data=article_metadata
-    )
+        response = requests.post(
+            url=f'https://api.medium.com/v1/users/{get_user_info()["data"]["id"]}/posts',
+            headers=headers,
+            timeout=10,
+            data=article_metadata,
+        )
+    except Exception as error_message:
+        print(error_message)
+        return False
 
     print(response.json())
-
+    print("Article posted")
     return True
 
-post_article(article_contents)
+
+def read_file(filename):
+    """
+    Read file from given path
+
+    Args:
+        filename: Path to file to be read
+
+    Returns:
+        contents of file
+    """
+    try:
+        with open(filename, "r", encoding="UTF-8") as file_contents:
+            contents = file_contents.read()
+    except FileNotFoundError:
+        print("File not found")
+        return None
+    except Exception as error_message:
+        print(error_message)
+        return None
+
+    return contents
+
+
+def main():
+    """
+    Main function
+    """
+    for filename in os.listdir("articles"):
+        if filename.endswith(".md"):
+            contents = read_file(f"articles/{filename}")
+            if contents:
+                post_article(contents, state="draft")
+            else:
+                print("No contents found")
+        else:
+            continue
+
+
+if __name__ == "__main__":
+    main()
